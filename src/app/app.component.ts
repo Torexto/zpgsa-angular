99de6f90bbe8c4b82b0cae5bd745ccfdc947cbe2
torexto <tomasztrela8@gmail.com>
2025-05-30 23:42:36 +0200

    analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {MapComponent} from './map/map.component';
+import { inject } from '@vercel/analytics';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,10 @@ import {MapComponent} from './map/map.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'zpgsa';
+
+  ngOnInit() {
+    inject();
+  }
 }
